5a77b0936b090a4fc4bd56d8dc748cea941b925f
Jared Giles <jared@aleph-one.net>
2008-11-23 00:32:42 -0600

    Removed "RESTfulness" of the contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,35 +1,17 @@
 class ContactsController < ApplicationController
   # GET /contacts
-  # GET /contacts.xml
   def index
     @contacts = Contact.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @contacts }
-    end
   end
 
   # GET /contacts/1
-  # GET /contacts/1.xml
   def show
     @contact = Contact.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @contact }
-    end
   end
 
   # GET /contacts/new
-  # GET /contacts/new.xml
   def new
     @contact = Contact.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @contact }
-    end
   end
 
   # GET /contacts/1/edit
@@ -38,48 +20,33 @@ class ContactsController < ApplicationController
   end
 
   # POST /contacts
-  # POST /contacts.xml
   def create
     @contact = Contact.new(params[:contact])
 
-    respond_to do |format|
-      if @contact.save
-        flash[:notice] = 'Contact was successfully created.'
-        format.html { redirect_to(@contact) }
-        format.xml  { render :xml => @contact, :status => :created, :location => @contact }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @contact.errors, :status => :unprocessable_entity }
-      end
+    if @contact.save
+      flash[:notice] = 'Contact was successfully created.'
+      redirect_to @contact
+    else
+      render :action => "new"
     end
   end
 
   # PUT /contacts/1
-  # PUT /contacts/1.xml
   def update
     @contact = Contact.find(params[:id])
 
-    respond_to do |format|
-      if @contact.update_attributes(params[:contact])
-        flash[:notice] = 'Contact was successfully updated.'
-        format.html { redirect_to(@contact) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @contact.errors, :status => :unprocessable_entity }
-      end
+    if @contact.update_attributes(params[:contact])
+      flash[:notice] = 'Contact was successfully updated.'
+      redirect_to @contact
+    else
+      render :action => "edit"
     end
   end
 
   # DELETE /contacts/1
-  # DELETE /contacts/1.xml
   def destroy
     @contact = Contact.find(params[:id])
     @contact.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(contacts_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to contacts_url
   end
 end
